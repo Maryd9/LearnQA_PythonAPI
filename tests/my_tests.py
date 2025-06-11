ecdf6b9c5f03624d12e4d8f9712a2070bae1d984
masha <dobrynina_mg@magnit.ru>
2025-06-11 12:37:06 +0500

    Тест запроса на метод cookie

--- tests/my_tests.py
+++ tests/my_tests.py
@@ -1,6 +1,19 @@
+import requests
+
+
 class TestExample:
 
     def test_short_phrase(self):
         phrase = input("Set a phrase: ")
         expected_len = 15
         assert len(phrase) < expected_len, f"The number of characters is equal to or greater than {expected_len}"
+
+    def test_request_method_cookie(self):
+        response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
+        assert response.status_code == 200, "Wrong response code"
+        cookie = response.cookies
+        print(cookie)
+        assert "HomeWork" in cookie, "There is no cookie 'HomeWork' in the response"
+        expected_value = "hw_value"
+        actual_value = cookie.get('HomeWork')
+        assert actual_value == expected_value, f"Wrong cookie value {cookie}"
