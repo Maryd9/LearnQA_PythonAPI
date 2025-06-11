da6c380710097f44e212df7f8d5a5c35629b03f6
masha <dobrynina_mg@magnit.ru>
2025-06-11 12:25:34 +0500

    Тест запроса на метод cookie

--- tests/test_short_phrase.py
+++
@@ -1,6 +0,0 @@
-class TestExample:
-
-    def test_short_phrase(self):
-        phrase = input("Set a phrase: ")
-        expected_len = 15
-        assert len(phrase) < expected_len, f"The number of characters is equal to or greater than {expected_len}"
