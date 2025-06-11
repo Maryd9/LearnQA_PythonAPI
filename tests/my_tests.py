bd10dce0975a20deabd6b09a9a2b56a0e465f06b
masha <dobrynina_mg@magnit.ru>
2025-06-11 12:43:50 +0500

    Тест

--- tests/my_tests.py
+++ tests/my_tests.py
@@ -11,9 +11,11 @@ class TestExample:
     def test_request_method_cookie(self):
         response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
         assert response.status_code == 200, "Wrong response code"
+
         cookie = response.cookies
         print(cookie)
         assert "HomeWork" in cookie, "There is no cookie 'HomeWork' in the response"
+
         expected_value = "hw_value"
         actual_value = cookie.get('HomeWork')
-        assert actual_value == expected_value, f"Wrong cookie value {cookie}"
+        assert actual_value == expected_value, f"Wrong cookie value {actual_value}"
