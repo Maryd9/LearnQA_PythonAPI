069acaa1c37a1e8e0717bf18e2f20e023dbab492
masha <dobrynina_mg@magnit.ru>
2025-06-16 14:08:59 +0500

    Все тесты вместе

--- tests/my_tests.py
+++ tests/my_tests.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime, timezone
 
 
 class TestExample:
@@ -19,3 +20,28 @@ class TestExample:
         expected_value = "hw_value"
         actual_value = cookie.get('HomeWork')
         assert actual_value == expected_value, f"Wrong cookie value {actual_value}"
+
+    def test_request_method_header(self):
+        response = requests.get("https://playground.learnqa.ru/api/homework_header")
+        assert response.status_code == 200, "Wrong response code"
+
+        headers = response.headers
+        print(headers)
+
+        now_datetime = datetime.now(timezone.utc)
+        formatted_datetime = now_datetime.strftime('%a, %d %b %Y %H:%M:%S GMT')
+
+        expected_headers = {"Date": formatted_datetime,
+                            "Content-Type": "application/json",
+                            "Content-Length": "15",
+                            "Connection": "keep-alive",
+                            "Keep-Alive": "timeout=10",
+                            "Server": "Apache",
+                            "x-secret-homework-header": "Some secret value",
+                            "Cache-Control": "max-age=0",
+                            "Expires": formatted_datetime}
+        missed_headers = [header for header in expected_headers if header not in headers]
+        assert not missed_headers, f"There is no header {', '.join(missed_headers)} in the response"
+
+        for header, expected_value in expected_headers.items():
+            assert headers[header] == expected_value, f"The header {header} has unexpected value {headers[header]}"
